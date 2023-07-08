ad69d5457bc92eb791791964b4c314fa68e0892b
Ashwanidubeyiitb <dubeyash2004@gmail.com>
2023-07-09 01:30:33 +0530

    crud

--- arrayproject.js
+++ arrayproject.js
@@ -1,16 +1,38 @@
-const todolist = [];
-for(let i=0; i<todolist.length; i++){
-    const todo=todolist[i];
+const todolist = [{
+    name: 'dinner',
+    duedate:'2023-12-22'
+}];
+rendertodolit();
+function rendertodolit() {
 
+
+    let todohtnl = '';
+    for (let i = 0; i < todolist.length; i++) {
+        const todoobject = todolist[i];
+        const name=todoobject.name;
+        //const {name,duedate}= todoobject;
+        const duedate=todoobject.duedate;
+        const html = 
+        `<p>
+        ${name} ${duedate}
+        <button onclick="todolist.slice(${i},1);
+        rendertodolit()";
+        >delete</button>
+        </p>`
+        todohtnl += html;
+    }
+    console.log(todohtnl);
+    document.querySelector('.jstodolist').innerHTML = todohtnl;
 }
 function addtodo() {
-    const inputelement=document.querySelector('.jsnamein');
-    const name=inputelement.value;
+    const inputelement = document.querySelector('.jsnamein');
+    const name = inputelement.value;
 
     todolist.push(name);
     console.log(todolist);
 
 
-    inputelement.value='';
+    inputelement.value = '';
+    rendertodolit();
 
 }
